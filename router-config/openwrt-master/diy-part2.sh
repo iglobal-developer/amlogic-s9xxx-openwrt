1f0e96859622ac64e4c76a6aeb6c4da7e0c009e0
iglobal-developer <69662383+iglobal-developer@users.noreply.github.com>
2022-07-07 00:03:32 +0700

    Updated v2r's repos

--- router-config/openwrt-master/diy-part2.sh
+++ router-config/openwrt-master/diy-part2.sh
@@ -46,5 +46,5 @@ svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/
 # ------------------------------- Other ends -------------------------------
 rm -rf package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-git clone https://github.com/kuoruan/openwrt-v2ray.git package/v2ray-core
+git clone https://github.com/iglobal-developer/openwrt-v2ray.git package/v2ray-core
 git clone -b luci2 https://github.com/kuoruan/luci-app-v2ray.git package/luci-app-v2ray
