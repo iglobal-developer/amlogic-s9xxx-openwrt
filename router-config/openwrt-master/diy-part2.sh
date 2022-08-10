29d0a900f782c4ed580311c363207324bcf3e954
iglobal-developer <69662383+iglobal-developer@users.noreply.github.com>
2022-08-10 10:29:17 +0700

    added new version of lualanes

--- router-config/openwrt-master/diy-part2.sh
+++ router-config/openwrt-master/diy-part2.sh
@@ -48,3 +48,4 @@ rm -rf package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone https://github.com/kuoruan/openwrt-v2ray.git package/v2ray-core
 git clone -b luci2 https://github.com/kuoruan/luci-app-v2ray.git package/luci-app-v2ray
+git clone https://github.com/iglobal-developer/lualanes.git package/lualanes
