0a82919957a6c80ba8935cc0e294b981f88704b0
iGlobal Dev <develop.iglobal@gmail.com>
2022-08-22 08:25:06 +0000

    added dependent packs

--- router-config/openwrt-master/diy-part2.sh
+++ router-config/openwrt-master/diy-part2.sh
@@ -50,6 +50,7 @@ rm -rf package/lean/luci-theme-argon
 git clone https://github.com/iglobal-developer/luci-theme-argon.git package/lean/luci-theme-argon
 rm -rf package/luci
 git clone https://github.com/iglobal-developer/luci.git package/luci
+git clone -b master --depth 1 https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
 git clone https://github.com/iglobal-developer/openwrt-v2ray.git package/v2ray-core
 git clone "https://iglobal-developer:"$GITHUB_CHECKOUT"@github.com/iglobal-developer/vsocks.git" package/vsocks-app-vproxy
 # git clone -b luci2 https://github.com/kuoruan/luci-app-v2ray.git package/luci-app-v2ray
