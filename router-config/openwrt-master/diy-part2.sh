b4fdcb876dd255eb6bd533ae95b2f41d4d868c0c
iGlobal Dev <develop.iglobal@gmail.com>
2022-10-06 10:50:47 +0000

    just did remove footprint stuff

--- router-config/openwrt-master/diy-part2.sh
+++ router-config/openwrt-master/diy-part2.sh
@@ -12,11 +12,16 @@
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Add the default password for the 'root' user（Change the empty password to 'password'）
-sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+sed -i 's/nobody:\*:0:0:99999:7:::/nobody:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Set etc/openwrt_release
 grep -v "DISTRIB_DESCRIPTION=" package/base-files/files/etc/openwrt_release > tmpFile && mv tmpFile package/base-files/files/etc/openwrt_release
-echo "VSocks V1.0" >>package/base-files/files/etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='VSocks V1.0'" >>package/base-files/files/etc/openwrt_release
+grep -v "luciname    = " package/base-files/files/usr/lib/lua/luci/version.lua  > tmpFile  && mv tmpFile package/base-files/files/usr/lib/lua/luci/version.lua
+echo "luciname    = VPROXY" >> package/base-files/files/usr/lib/lua/luci/version.lua
+grep -v "luciversion = " package/base-files/files/usr/lib/lua/luci/version.lua  > tmpFile  && mv tmpFile package/base-files/files/usr/lib/lua/luci/version.lua
+echo "luciversion = V1.0" >> package/base-files/files/usr/lib/lua/luci/version.lua
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
 echo "DISTRIB_SOURCECODE='openwrt.master'" >>package/base-files/files/etc/openwrt_release
 
